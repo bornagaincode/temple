e5fb4e26b40d40e110cb01b136df34b0209d08ab
Igor Putina <iputina@mixbook.com>
2021-02-21 22:47:20 +0200

    Convert component to class component with state

--- src/Announcement.tsx
+++ src/Announcement.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Announcement.css';
 
-function Announcement() {
-  return (
-    <div className="Announcement">
-      <div className="Announcement--content">
-          Our Church honors all possible COVID-19 precautions for your safety.
+interface IProps {
+}
+
+interface IState {
+  text: String;
+}
+
+class Announcement extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      text: "Our Church honors all possible COVID-19 precautions for your safety."
+    }
+  }
+
+  render() {
+    return (
+      <div className="Announcement">
+        <div className="Announcement--content">
+          {this.state.text}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Announcement;
