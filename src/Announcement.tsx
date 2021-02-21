ba2584d24fb89b2b180e36247c7b1a561971b228
Igor Putina <iputina@mixbook.com>
2021-02-21 21:53:32 +0200

    Static announcement component

---
+++ src/Announcement.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Announcement.css';
+
+function Announcement() {
+  return (
+    <div className="Announcement">
+      <div className="Announcement--content">
+          Our Church honors all possible COVID-19 precautions for your safety.
+      </div>
+    </div>
+  );
+}
+
+export default Announcement;
