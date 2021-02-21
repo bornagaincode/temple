249d10300758c91e339c2da542afa164fda8a9ee
Igor Putina <iputina@mixbook.com>
2021-02-21 21:56:19 +0200

    Static announcement component (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
+import Announcement from './Announcement';
 
 function App() {
   return (
     <div className="App">
+      <Announcement></Announcement>
       <header className="App-header">
-        <p>
-          Blank application.
-        </p>
+        Blank Application
       </header>
     </div>
   );
