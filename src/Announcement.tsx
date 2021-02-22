117a21b778a41685d8d6447aede4115062a318fe
Igor Putina <iputina@mixbook.com>
2021-02-22 20:03:05 +0200

    Refactor to use functional components

--- src/Announcement.tsx
+++ src/Announcement.tsx
@@ -1,31 +1,20 @@
-import React from 'react';
-import './Announcement.css';
+import React, { useState, useEffect } from "react";
+import "./Announcement.css";
 
-interface IProps {
-}
+function Announcement(): JSX.Element {
+  const [text, setText] = useState("Announcement");
 
-interface IState {
-  text: String;
-}
-
-class Announcement extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-
-    this.state = {
-      text: "Our Church honors all possible COVID-19 precautions for your safety."
-    }
-  }
-
-  render() {
-    return (
-      <div className="Announcement">
-        <div className="Announcement--content">
-          {this.state.text}
-        </div>
-      </div>
+  useEffect(() => {
+    setText(
+      "Our Church honors all possible COVID-19 precautions for your safety."
     );
-  }
+  }, []);
+
+  return (
+    <div className="Announcement">
+      <div className="Announcement--content">{text}</div>
+    </div>
+  );
 }
 
 export default Announcement;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import './App.css';
-import Announcement from './Announcement';
+import React from "react";
+import "./App.css";
+import Announcement from "./Announcement";
 
 function App() {
   return (
     <div className="App">
-      <Announcement></Announcement>
-      <header className="App-header">
-        Blank Application
-      </header>
+      <Announcement />
+      <header className="App-header">Blank Application</header>
     </div>
   );
 }
