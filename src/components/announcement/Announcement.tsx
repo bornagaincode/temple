546f17a7218d58c278bd5515801ff51a9a3bffba
Igor Putina <iputina@mixbook.com>
2021-03-05 18:08:38 +0200

    Refactor announcement to use editable text

--- src/components/announcement/Announcement.tsx
+++ src/components/announcement/Announcement.tsx
@@ -1,45 +1,14 @@
-import React, { useState, useRef, useEffect } from "react";
-import AnnouncementEditor from "./AnnouncementEditor";
-import AnnouncementText from "./AnnouncementText";
+import React from "react";
 import "./Announcement.css";
+import EditableText from "../editable_text/EditableText";
 
 function Announcement(): JSX.Element {
-  const root = useRef<HTMLDivElement>(null);
-  const [text, setText] = useState(
-    "Our Church honors all possible COVID-19 precautions for your safety."
-  );
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
-  const onMouseEnterHandler = () => setIsEditMode(true);
-  const onMouseLeaveHandler = () => (isFocused ? false : setIsEditMode(false));
-
-  useEffect(() => {
-    const listenForOutsideClicks = (e: MouseEvent) =>
-      root.current?.contains(e.target as Node) ? null : setIsEditMode(false);
-
-    document.body.addEventListener("click", listenForOutsideClicks);
-
-    return () => {
-      document.body.removeEventListener("click", listenForOutsideClicks);
-    };
-  }, []);
+  const text =
+    "Our Church honors all possible COVID-19 precautions for your safety.";
 
   return (
-    <div
-      ref={root}
-      id="Announcement"
-      onMouseEnter={onMouseEnterHandler}
-      onMouseLeave={onMouseLeaveHandler}
-    >
-      {isEditMode ? (
-        <AnnouncementEditor
-          text={text}
-          setAnnouncementText={setText}
-          setIsFocused={setIsFocused}
-        />
-      ) : (
-        <AnnouncementText text={text} />
-      )}
+    <div id="Announcement">
+      <EditableText placeholder={text} />
     </div>
   );
 }
