e8d13c2fbf2995aed8a63ff433ebb481c740caac
Igor Putina <iputina@mixbook.com>
2021-03-10 16:04:52 +0200

    Implement text editing with `medium-editor` (#29)
    * Add `medium-editor` to packages
    
    * Implement text editor using `medium-editor` library
    
    * Add multiple editable text elements
    
    * Use the same text editor for announcements

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,19 +1,29 @@
 import React from "react";
+import TextEditor from "../text_editor/TextEditor";
 import "./Hero.css";
 
 function Hero(): JSX.Element {
+  const headlineTextContent = "Everyone has a story";
+  const subTextContent =
+    "We are the International Christian Fellowship in Moldova. " +
+    "It is through the sharing of our <b>stories</b> that <b>meaningful relationships</b> are formed.";
+  const tipTextContent =
+    "Learn about our <b>services</b> and <b>get directions</b>";
+
   return (
     <section id="Hero">
-      <h2 id="Hero--headline">Everyone has a story</h2>
+      <h2 id="Hero--headline">
+        <TextEditor content={headlineTextContent} width={600}></TextEditor>
+      </h2>
       <p id="Hero--sub">
-        We are the International Christian Fellowship in Moldova. It is through
-        the sharing of our <strong>stories</strong> that{" "}
-        <strong>meaningful relationships</strong> are formed.
+        <TextEditor content={subTextContent} width={800}></TextEditor>
       </p>
       <button id="Hero--cta" className="btn btn-lg btn-primary">
         Join us on Sunday
       </button>
-      <p id="Hero--tip">Learn about our services and get directions</p>
+      <p id="Hero--tip">
+        <TextEditor content={tipTextContent} width={500}></TextEditor>
+      </p>
     </section>
   );
 }
---
+++ src/components/text_editor/TextEditor.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from "react";
+import TextFrame from "./TextFrame";
+import MediumEditor from "medium-editor";
+import "./TextEditor.css";
+
+interface ITextProps {
+  content: string;
+  className?: string;
+  width?: number;
+  maxWidth?: number;
+  minWidth?: number;
+}
+
+function TextEditor(props: ITextProps): JSX.Element {
+  const { content, width, maxWidth, minWidth } = props;
+  const root = useRef(null);
+
+  useEffect(() => {
+    // Instantiate editor
+    const editor = new MediumEditor(root.current!, {
+      toolbar: {
+        buttons: ["bold", "italic", "underline", "anchor"],
+      },
+    });
+
+    // Initialize with HTML content
+    editor.setContent(content);
+
+    // Save changes
+    editor.subscribe("editableInput", (event: Event) => {
+      // Post to server or update state here
+    });
+  });
+
+  return (
+    <TextFrame
+      width={width || 800}
+      minWidth={500 || minWidth}
+      maxWidth={1400 || maxWidth}
+      align={"center"}
+    >
+      <div ref={root} className="TextEditor"></div>
+    </TextFrame>
+  );
+}
+
+export default TextEditor;
